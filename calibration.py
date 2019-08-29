079ce813f3a638c366f65488ebb76849cd9b355e
Joab <jlsn1@ifal.edu.br>
2019-08-29 12:27:09 -0300

    Reprojection: calcula erro de reprojeção

--- calibration.py
+++ calibration.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import numpy as np
 import cv2 as cv
 import glob
@@ -5,13 +7,25 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+cbrow = 7
+cbcol = 6
+
+
+def re_projection_error(dist, imgpoints, mtx, objpoints, rvecs, tvecs) -> float:
+    mean_error = 0
+    for i in range(len(objpoints)):
+        imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2) / len(imgpoints2)
+        mean_error += error
+    return mean_error
+
 
 def main():
     # termination criteria
-    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 39, 0.001)
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((6 * 7, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+    objp = np.zeros((cbrow * cbcol, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:cbcol, 0:cbrow].T.reshape(-1, 2)
     # Arrays to store object points and image points from all the images.
     objpoints = []  # 3d point in real world space
     imgpoints = []  # 2d points in image plane.
@@ -22,17 +36,32 @@ def main():
         img = cv.imread(fname[0])
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         # Find the chess board corners
-        ret, corners = cv.findChessboardCorners(gray, (7, 7), None)
+        ret, corners = cv.findChessboardCorners(gray, (cbcol, cbrow), None)
         # If found, add object points, image points (after refining them)
         if ret == True:
             objpoints.append(objp)
             corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
             imgpoints.append(corners)
             # Draw and display the corners
-            cv.drawChessboardCorners(img, (7, 7), corners2, ret)
+            cv.drawChessboardCorners(img, (cbcol, cbrow), corners2, ret)
             cv.imshow('img', img)
             cv.waitKey(500)
     cv.destroyAllWindows()
+
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    img = cv.imread(os.path.join(images_dir, 'left12.jpg'))
+    h, w = img.shape[:2]
+    newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+    # undistort
+    mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w, h), 5)
+    dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+    # crop the image
+    x, y, w, h = roi
+    dst = dst[y:y + h, x:x + w]
+    cv.imwrite('calibresult.png', dst)
+    mean_error = re_projection_error(dist, imgpoints, mtx, objpoints, rvecs, tvecs)
+    print("total error: {}".format(mean_error / len(objpoints)))
 
 
 if __name__ == '__main__':
